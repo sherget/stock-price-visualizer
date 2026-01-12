f5ff38d23621bf99798e8a11f5df4cf0781c2dc8
sherget <sherget0x5d@gmail.com>
2026-01-12 12:47:33 +0100

    wip: experimenting with gnuplot

--- src/main.c
+++ src/main.c
@@ -1,4 +1,6 @@
+#define _POSIX_C_SOURCE 200809L
 #include "stock_data.h"
+#include <stdio.h>
 
 const int NUMBER_OF_ENTRIES = 20;
 
@@ -6,5 +8,26 @@ int main() {
     StockDataArray* data = stock_data_create(NUMBER_OF_ENTRIES);
     stock_data_generate_dummy_data(data);
     stock_data_print_entries(data);
+
+    FILE *gnuplot_pipe = popen("gnuplot -persistent", "w");
+    if (gnuplot_pipe == NULL) {
+        fprintf(stderr, "Error opening gnuplot pipe\n");
+        return 1;
+    }
+
+    fprintf(gnuplot_pipe, "set title 'Stockprice over Time'\n");
+    fprintf(gnuplot_pipe, "set xlabel 'Datetime'\n");
+    fprintf(gnuplot_pipe, "set ylabel 'Price'\n");
+    fprintf(gnuplot_pipe, "plot '-' using 1:2 with lines smooth bezier\n");
+
+    for (int i = 0; i < NUMBER_OF_ENTRIES; i++) {
+        fprintf(gnuplot_pipe, "%ld %.2f\n", data->entries[i].timestamp, data->entries[i].price_in_cents / 100.0);
+    }
+
+    fprintf(gnuplot_pipe, "e\n");
+    fflush(gnuplot_pipe);
+    pclose(gnuplot_pipe);
+
+    return 0;
 }
 
